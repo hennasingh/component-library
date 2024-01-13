426f1fc17d9ab2c0c07c017b42c55ff314bb94a3
Henna Singh <hennasingh04@gmail.com>
2024-01-13 17:49:35 +0000

    edited relative position and added hover effect to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,13 +4,13 @@ import { IoCloudUploadOutline } from "react-icons/io5";
 export default function Card({children}) {
 
     return (
-        <>
+        <div className="div-card">
             <div className='card-upload'>
                 <IoCloudUploadOutline />   
             </div>              
             <div className='card-body' >
                 {children}
             </div>
-        </>
+        </div>
     )
 }
